7f31880a4437cc4eef147dc84e2e1a8fdd4fa24c
Kev Mcgrady <kevinmcgrady47@gmail.com>
2024-06-23 11:20:13 +0100

    @feat/created 404 page

--- src/app/(root)/[...not-found]/page.tsx
+++ src/app/(root)/[...not-found]/page.tsx
@@ -1,5 +1,22 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { buttonVariants } from '@/components/ui/button';
+import { urls } from '@/constants/urls';
+import { cn } from '@/lib/utils';
+
 function NotFoundPage() {
-  return <h1>Page not found</h1>;
+  return (
+    <div className='container flex flex-col items-center'>
+      <Image src='/images/404.svg' alt='Not Found' height={450} width={450} />
+      <Link
+        className={cn(buttonVariants(), 'mb-32')}
+        href={urls.dashboard.root}
+      >
+        Back to safety
+      </Link>
+    </div>
+  );
 }
 
 export default NotFoundPage;
