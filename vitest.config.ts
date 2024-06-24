223f98e3f2407eaf42958cb78f4f8b6cf48b7cf8
Kev McGrady <kevinmcgrady47@gmail.com>
2024-06-24 19:29:48 +0100

    @tests/billing components

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
     environment: 'jsdom',
+    globals: true,
     coverage: {
       provider: 'istanbul',
       exclude: [
